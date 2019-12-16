2adbba6cf3ac19795457ec32e67d816aad9ddc88
PawelRadawiec <pawel.radaw@gmail.com>
2019-12-16 19:20:35 +0100

    lombok dependency / handle Todo with lombok

--- src/main/java/com/info/todobackend/model/Todo.java
+++ src/main/java/com/info/todobackend/model/Todo.java
@@ -1,10 +1,15 @@
 package com.info.todobackend.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "todos")
+@Getter @Setter @NoArgsConstructor
 public class Todo {
 
     @Id
@@ -19,40 +24,4 @@ public class Todo {
 
     @Column(name = "target_type")
     private LocalDate targetType;
-
-    public Todo(){
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Boolean getDone() {
-        return done;
-    }
-
-    public void setDone(Boolean done) {
-        this.done = done;
-    }
-
-    public LocalDate getTargetType() {
-        return targetType;
-    }
-
-    public void setTargetType(LocalDate targetType) {
-        this.targetType = targetType;
-    }
 }
