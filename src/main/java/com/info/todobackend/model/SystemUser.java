464010c159b75e22507103f93e326db904d5966f
PawelRadawiec <pawel.radaw@gmail.com>
2020-02-04 20:18:09 +0100

    user error handling

--- src/main/java/com/info/todobackend/model/SystemUser.java
+++ src/main/java/com/info/todobackend/model/SystemUser.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "system_user")
@@ -15,18 +16,23 @@ public class SystemUser {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotEmpty
     @Column(name = "first_name")
     private String firstName;
 
+    @NotEmpty
     @Column(name = "last_name")
     private String lastName;
 
+    @NotEmpty
     @Column(unique = true)
     private String login;
 
+    @NotEmpty
     @Column(unique = true)
     private String email;
 
+    @NotEmpty
     @Column()
     private String password;
 
